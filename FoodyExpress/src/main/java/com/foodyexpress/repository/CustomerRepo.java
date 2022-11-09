45e9781aed11630242d184f1aa2d2a4670cfef98
sanajitjana <dev.sanajitjana@gmail.com>
2022-11-10 03:54:09 +0530

    Create customer repository

--- FoodyExpress/src/main/java/com/foodyexpress/repository/CustomerRepo.java
+++ FoodyExpress/src/main/java/com/foodyexpress/repository/CustomerRepo.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.foodyexpress.model.Customer;
 
 @Repository
-public interface CustomerRepo extends JpaRepository<Customer, Integer> {
+public interface CustomerRepo extends JpaRepository<Customer, String> {
 
 }
