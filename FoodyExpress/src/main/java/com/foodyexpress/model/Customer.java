d8de95095e533973bc14908a280adf619ace8949
sanajitjana <dev.sanajitjana@gmail.com>
2022-11-10 03:39:43 +0530

    Made customer model

--- FoodyExpress/src/main/java/com/foodyexpress/model/Customer.java
+++ FoodyExpress/src/main/java/com/foodyexpress/model/Customer.java
@@ -1,100 +1,42 @@
 package com.foodyexpress.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Customer {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer customerId;
-
 	private String firstName;
 	private String lastName;
 	private Integer age;
 	private String gender;
 	private String mobileNumber;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	private Address address;
+
+	@JsonIgnore
+	@OneToOne(cascade = CascadeType.ALL)
+	private FoodCart foodCart;
+
 	private String email;
-
-	public Integer getCustomerId() {
-		return customerId;
-	}
-
-	public void setCustomerId(Integer customerId) {
-		this.customerId = customerId;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-
-	public String getGender() {
-		return gender;
-	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
-	public String getMobileNumber() {
-		return mobileNumber;
-	}
-
-	public void setMobileNumber(String mobileNumber) {
-		this.mobileNumber = mobileNumber;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	@Override
-	public String toString() {
-		return "Customer [customerId=" + customerId + ", firstName=" + firstName + ", lastName=" + lastName + ", age="
-				+ age + ", gender=" + gender + ", mobileNumber=" + mobileNumber + ", email=" + email + "]";
-	}
-
-	public Customer(Integer customerId, String firstName, String lastName, Integer age, String gender,
-			String mobileNumber, String email) {
-		super();
-		this.customerId = customerId;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.age = age;
-		this.gender = gender;
-		this.mobileNumber = mobileNumber;
-		this.email = email;
-	}
-
-	public Customer() {
-		// TODO Auto-generated constructor stub
-	}
 
 }
